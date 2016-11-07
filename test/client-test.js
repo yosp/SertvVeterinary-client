64c11302ecb97d532a6b82a8c499a4a32a40d850
Yeison Segura <yeisonsegura@Yeisons-MacBook-Pro.local>
2016-11-06 22:21:16 -0400

    Client class are created and methods for user and auth api are created and tested

---
+++ test/client-test.js
@@ -0,0 +1,115 @@
+'use strict'
+
+const test = require('ava')
+const nock = require('nock')
+const sertvveterinary = require('../')
+const fixtures = require('./fixtures')
+
+let options = {
+  endpoints: {
+    appointment: 'http://sertvveterinary.com/appointment',
+    aprecord: 'http://sertvveterinary.com/aprecord',
+    auth: 'http://sertvveterinary.com/auth',
+    bills: 'http://sertvveterinary.com/bills',
+    billdetail: 'http://sertvveterinary.com/billdetail',
+    client: 'http://sertvveterinary.com/client',
+    ethnicities: 'http://sertvveterinary.com/ethnicities',
+    internment: 'http://sertvveterinary.com/internment',
+    interecord: 'http://sertvveterinary.com/interecord',
+    laboratory: 'http://sertvveterinary.com/laboratory',
+    medicine: 'http://sertvveterinary.com/medicine',
+    pet: 'http://sertvveterinary.com/pet',
+    products: 'http://sertvveterinary.com/products',
+    race: 'http://sertvveterinary.com/race',
+    users: 'http://sertvveterinary.com/users'
+  }
+}
+
+test.beforeEach(t => {
+  t.context.client = sertvveterinary.createClient(options)
+})
+
+test('auth', async t => {
+  const client = t.context.client
+
+  let credentials = {
+    username: 'freddier',
+    password: 'pl4tzi'
+  }
+
+  let token = 'xxx-xxx-xxx'
+
+  nock(options.endpoints.auth)
+    .post('/', credentials)
+    .reply(200, token)
+
+  let result = await client.auth(credentials.username, credentials.password)
+
+  t.deepEqual(result, token)
+})
+
+test('getuser', async t => {
+  const client = t.context.client
+
+  let user = fixtures.getUser()
+
+  nock(options.endpoints.users)
+    .get(`/${user.username}`)
+    .reply(200, user)
+
+  let result = await client.getUser(user.username)
+
+  t.deepEqual(result, user)
+})
+
+test('saveUser', async t => {
+  const client = t.context.client
+
+  let user = fixtures.getUser()
+  let newUser = {
+    username: user.username,
+    name: user.name,
+    email: 'yosp@gmail.com',
+    password: 'blablaba'
+  }
+
+  nock(options.endpoints.users)
+    .post('/', newUser)
+    .reply(201, user)
+
+  let result = await client.saveUser(newUser)
+
+  t.deepEqual(result, user)
+})
+
+test('updateUser', async t => {
+  const client = t.context.client
+
+  let user = fixtures.getUser()
+  let token = 'xxx-xxx-xxx'
+
+  nock(options.endpoints.users, {
+    reqheaders: {
+      'Authorization': `Bearer ${token}`}
+  })
+    .post('/update', user)
+    .reply(201, user)
+
+  let result = await client.updateUser(user, token)
+
+  t.deepEqual(result, user)
+})
+
+test('saveClient', async t => {
+  const client = t.context.client
+
+  let apClient = fixtures.getClient()
+
+  nock(options.endpoints.client)
+    .post('/', apClient)
+    .reply(201, apClient)
+
+  let result = await client.saveClient(apClient)
+
+  t.deepEqual(result, apClient)
+})
---
+++ test/fixtures/index.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const uuid = require('uuid-base62')
+
+const fixtures = {
+  getUser () {
+    return {
+      id: uuid.uuid(),
+      name: 'A random user',
+      username: `user_${uuid.v4()}`,
+      createdAt: new Date().toString()
+    }
+  },
+
+  getClient () {
+    return {
+      id: uuid.uuid(),
+      fullname: 'Yeison Segura',
+      gender: 'Hombre',
+      direction: 'Calle 1ra #4',
+      email: 'yeisp1011@gmail.com',
+      phone: '809-414-8433',
+      phone2: '829-926-6545'
+    }
+  }
+}
+
+module.exports = fixtures
